ca4e22dc27ec1735b0e9bc41c496fa228d311d11
jaedyndant <142104295+jaedyndant@users.noreply.github.com>
2024-03-18 13:24:02 +0100

    fixed

--- shopee_form.php
+++ shopee_form.php
@@ -1,52 +1,7 @@
 <!doctype html>
 <html class="no-js" lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shopee | Product Details</title>
-    <link rel="icon" href="./favicon.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-        rel="stylesheet">
-    <script src="https://kit.fontawesome.com/1232e6a3df.js" crossorigin="anonymous"></script>
-    <meta name="description" content="">
-    <link rel="apple-touch-icon" href="icon.png">
-    <meta name="theme-color" content="#fafafa">
-</head>
-
-<body>
-    <div class="container">
-        <div class="navbar">
-            <div class="logo">
-                <nav>
-                    <ul>
-                        <li><a
-                                href="http://127.0.0.1:5500/eindopdrachten-webshop-overzicht-bestelformulier-jaedyndant-main/shopee_front.php"><img
-                                    src="assets/img/img/Shopee_Img/images/logoblack.png" width="200" alt="logo"></a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            <nav>
-                <ul id="MenuItems">
-                    <li><a
-                            href="http://127.0.0.1:5500/eindopdrachten-webshop-overzicht-bestelformulier-jaedyndant-main/shopee_front.php">Home</a>
-                    </li>
-                    <li><a
-                            href="http://127.0.0.1:5500/eindopdrachten-webshop-overzicht-bestelformulier-jaedyndant-main/products.php">Products</a>
-                    </li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Contact</a></li>
-                    <li><a href="">Account</a></li>
-                </ul>
-            </nav>
-            <img src="assets/img/img/Shopee_Img/images/cart.png" width="30" height="30">
-            <img src="assets/img/img/Shopee_Img/images/menu.png" class="menu-icon" onclick="menutoggle()">
-        </div>
-    </div>
+<?php include 'Templates/header.php'; ?>
 
     <!DOCTYPE html>
     <html lang="en">
